819c97b17036a1218b80a1eea2056d062e6988dd
joao <joao-fischer@hotmail.com>
2025-04-11 15:53:00 -0300

    Add Menu and Pagina components with initial user data structure

---
+++ src/app/data/constants/usuarios.ts
@@ -0,0 +1,10 @@
+import { Usuario } from "@/core/model/Usuario";
+
+const usuarios: Usuario[] = [
+    {
+        id: 1,
+        email: "joao@fischer.com",
+        nome: "João Fischer",
+        senha: "123456",
+    }
+]
